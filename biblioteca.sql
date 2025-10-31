fc5727f73eb6043f75b6b74e7c8551d5d4a9b1ac
Bussita <santiagobussanich321@gmail.com>
2025-10-30 21:54:06 -0300

    ej 3 y 4 a b

---
+++ biblioteca.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE IF NOT EXISTS biblioteca_db;
+
+USE biblioteca_db;
+
+DROP TABLE IF EXISTS Autor;
+DROP TABLE IF EXISTS Libro;
+
+CREATE TABLE Autor (
+	id INT NOT NULL AUTO_INCREMENT,
+	nombre VARCHAR(30) NOT NULL,
+	apellido VARCHAR(30) NOT NULL,
+	nacionalidad VARCHAR(30) NOT NULL,
+	residencia VARCHAR(30) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Libro (
+	isbn INT NOT NULL,
+	titulo VARCHAR(40) NOT NULL,
+	editorial VARCHAR(30) NOT NULL,
+	precio FLOAT NOT NULL,
+	PRIMARY KEY (isbn)
+);
+
+CREATE TABLE Escribe (
+	id INT NOT NULL,
+	isbn INT NOT NULL,
+	año INT NOT NULL,
+	PRIMARY KEY (id, isbn),
+	FOREIGN KEY (id) REFERENCES Autor(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (isbn) REFERENCES Libro(isbn) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE INDEX titulo_idx ON Libro(titulo);
+CREATE INDEX apellido_idx ON Autor(apellido);
+
+INSERT INTO Autor (nombre, apellido, nacionalidad, residencia)
+VALUES ("Jorge Luis","Borges","Argentina","Pellegrini 270"),
+        ("Julio","Cortazar","Argentina","Moreno 1650"),
+        ("Gabriel Jose García","Márquez","Colombia","Bolívar 1204"),
+        ("Abelardo","Castillo","Argentina","Belgrano 200");
+
+INSERT INTO Libro
+VALUES  (1001, 'El Aleph', 'Emecé', 15000),
+        (1002, 'Cien años de soledad', 'Sudamericana', 18000),
+        (1003, 'Rayuela', 'Alfaguara', 16000),
+        (1004, 'Bases de datos', 'UNR', 20000);
+
+INSERT INTO Escribe
+VALUES  (1, 1001, 1945),
+        (2,1002,1963),
+        (3,1003,1982);
+
+UPDATE Autor SET residencia = "Buenos Aires" WHERE nombre = "Abelardo" AND apellido = "Castillo";
+
+UPDATE Libro SET precio = precio * 1.1 WHERE editorial = "UNR"
